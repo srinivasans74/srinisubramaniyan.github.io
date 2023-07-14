57f7a84b451663e436fa9532794023462cd85562
srinivasans74 <47769589+srinivasans74@users.noreply.github.com>
2023-07-14 13:52:34 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,5 +1,24 @@
 function generateMenu() {
     var menuCode = `
+    <table summary="Table for page layout." id="tlayout">
+    <tr valign="top">
+    <td id="layout-menu">
+    <div class="menu-category">About</div>
+    <div class="menu-item"><a href="index.html" class="current">Home</a></div>
+    <div class="menu-item"><a href="CV_January21 (1).pdf">CV</a></div>
+    <div class="menu-item"><a href="https://www.linkedin.com/in/srinivasan-subramaniyan22/">Linkedin</a></div>
+    <div class="menu-item"><a href="https://scholar.google.com/citations?user=luGswuwAAAAJ&hl=en&oi=ao">Google Scholar</a></div>
+    <div class="menu-item"><a href="Academics.html">Academics</a></div>
+    <div class="menu-category">Research</div>
+    <div class="menu-item"><a href="publications.html">Publications</a></div>
+    <div class="menu-item"><a href="awards.html">Awards</a></div>
+    <div class="menu-category">Work Experience</div>
+    <div class="menu-item"><a href="internship.html">Internships</a></div>
+    <div class="menu-item"><a href="fulltime.html">Fulltime</a></div>
+    <div class="menu-item"><a href="ta.html">TA&nbsp;duties</a></div>
+</td>
+</tr>
+</table>
        
     `;
 
